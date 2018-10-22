9c47020b893a4580ff1ba5167ad41d9fbb736570
Alan Eicker <alaneicker@hotmail.com>
2018-10-22 09:42:09 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HeaderModule } from './components/header/header.module';
-import { ConnectionStatusModule } from './components/connection-status/connection-status.module';
-
 import { HttpService } from './services/http.service';
 import { ConnectionStatusService } from './services/connection-status.service';
 
-import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
+import { ConnectionStatusModule } from './components/connection-status/connection-status.module';
 import { LogoModule } from './components/logo/logo.module';
 import { DbTableListModule } from './components/db-table-list/db-table-list.module';
 import { QueryEditorModule } from './components/query-editor/query-editor.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
