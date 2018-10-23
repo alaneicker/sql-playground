e5130e591823ac109bef1212cee4831ccf7ef5a0
Alan Eicker <alaneicker@gmail.com>
2018-10-23 17:52:57 -0500

    connection form refactor

--- src/app/components/db-connection-form/db-connection-form.component.ts
+++ src/app/components/db-connection-form/db-connection-form.component.ts
@@ -4,6 +4,9 @@ import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 import { ConnectionStatusService } from '../../services/connection-status.service';
 import { HttpService } from '../../services/http.service';
 
+import { environment as env } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
+
 @Component({
   selector: 'app-db-connection-form',
   templateUrl: './db-connection-form.component.html',
@@ -12,25 +15,16 @@ import { HttpService } from '../../services/http.service';
 export class DbConnectionFormComponent implements OnInit {
   @Output() submitted: EventEmitter<any> = new EventEmitter();
 
-  form: FormGroup;
+  connectionType = 'standard';
 
-  standardConnection =  new FormGroup({
-    host: new FormControl('', Validators.required),
-    user: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    port: new FormControl('', Validators.required),
-    database: new FormControl('', Validators.required),
-  });
-
-  socketConnection = new FormGroup({
-    host: new FormControl('localhost'),
+  form: FormGroup = new FormGroup({
+    host: new FormControl(this.connectionType === 'socket' ? 'localhost' : ''),
     socketPath: new FormControl('', Validators.required),
     user: new FormControl('', Validators.required),
+    port: new FormControl(''),
     password: new FormControl('', Validators.required),
     database: new FormControl('', Validators.required),
   });
-
-  connectionType = 'standard';
 
   constructor(
     private connectionStatusService: ConnectionStatusService,
@@ -38,21 +32,25 @@ export class DbConnectionFormComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.form = this.standardConnection;
   }
 
   switchConnectionType(connectionType: string) {
-    this.form = connectionType === 'socket' ? this.socketConnection : this.standardConnection;
     this.connectionType = connectionType;
+    if (connectionType === 'standard') {
+      this.form.get('port').setValidators(Validators.required);
+      this.form.get('host').setValidators(Validators.required);
+      this.form.get('socketPath').clearValidators();
+    } else {
+      this.form.get('socketPath').setValidators(Validators.required);
+      this.form.get('port').clearValidators();
+      this.form.get('host').clearValidators();
+    }
   }
 
   connect(form: NgForm) {
     if (form.valid) {
-      // We need some type of hash for the username and
-      // password that can be decrypted on the server
-
       this.httpService.post({
-        url: 'http://localhost:8080/api/create-connection',
+        url: `${env.apiUrl}/create-connection`,
         data: this.form.value,
       })
       .then(res => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://mysql-playground.herokuapp.com/api',
 };
